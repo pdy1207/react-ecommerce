b2baf56a620e4d62e47fe77a5ca57127c6d021ef
pdy1207 <pdyme1207@gmail.com>
2023-05-13 00:44:21 +0900

    Recharts 뿌리기 및 Show.jsx 가상 데이터

--- src/stores/homeStore.js
+++ src/stores/homeStore.js
@@ -3,7 +3,8 @@ import { create } from "zustand";
 import debounce from "../helpers/debounce";
 
 const homeStore = create((set) => ({
-  coins: [],
+  coins: [], // 검색기능
+  trending: [], // 되돌아가기
   query: "",
 
   // 검색기능
@@ -13,11 +14,25 @@ const homeStore = create((set) => ({
   },
 
   searchCoins: debounce(async () => {
-    const { query } = homeStore.getState();
-    const res = await axios.get(
-      `https://api.coingecko.com/api/v3/search?query=${query}`
-    );
-    console.log(res);
+    const { query, trending } = homeStore.getState();
+
+    if (query.length > 2) {
+      const res = await axios.get(
+        `https://api.coingecko.com/api/v3/search?query=${query}`
+      );
+      const coins = res.data.coins.map((coin) => {
+        return {
+          name: coin.name,
+          image: coin.large,
+          id: coin.id,
+        };
+      });
+
+      set({ coins });
+      console.log(res.data);
+    } else {
+      set({ coins: trending });
+    }
   }, 500),
 
   fetchCoins: async () => {
@@ -35,7 +50,7 @@ const homeStore = create((set) => ({
       };
     });
 
-    set({ coins });
+    set({ coins, trending: coins });
     // console.log(coins);
   },
 }));
